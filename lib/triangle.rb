066ef48b2c8bb79b73ec9b7590af31b1ce45f81a
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 22:51:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,13 +16,15 @@ class Triangle
         :isosceles
       else
         :scalene
+      end
     end
-  end
 
   def validate_triangle
     real_triangle = [(side1 + side2 > side3), (side1 + side3 > side2), (side2 + side3 > side1)]
     [side1, side2, side3].each { |s| real_triangle << false if s <= 0 }
     raise TriangleError if real_triangle.include?(false)
   end
+
+  class TriangleError < StandardError
 
 end

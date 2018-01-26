0ab30799d3678d4325a31be9905874fd623f3c1e
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 22:49:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,19 +9,21 @@ class Triangle
   end
 
   def kind
-    if side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 > side3 || side2 + side3 > side1 || side1 + side3 > side2
-      raise TriangleError
-    else
+    if side1 > 0 && side2 > 0 && side3 > 0
+    end
       if side1 == side2 && side2 == side3
-        :equilateral
-      elsif side1 == side2 || side2 == side3 || side1 == side3
-        :isosceles
-      else
-        :scalene
-      end
+      :Equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
     end
   end
-end
 
-  class TriangleError < StandardError
+  def validate_triangle
+    real_triangle = [(side1 + side2 > side3), (side1 + side3 > side2), (side2 + side3 > side1)]
+    [side1, side2, side3].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
   end
+
+end

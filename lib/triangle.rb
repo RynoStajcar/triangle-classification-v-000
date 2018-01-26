39737fa6eb0393098e2d00740227b3fd1d3a120a
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 22:41:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,8 +21,8 @@ class Triangle
 
 
     def validate_triangle
-      real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-      [a, b, c].each { |s| real_triangle << false if s <= 0 }
+      real_triangle = [(side1 + side2 > side3), (side1 + side3 > side2), (side2 + side3 > side1)]
+      [side1, side2, side3].each { |s| real_triangle << false if s <= 0 }
       raise TriangleError if real_triangle.include?(false)
     end
 end

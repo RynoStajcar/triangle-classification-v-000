1d8a2d72810950974fbca80d839cbb9891ed4045
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 22:37:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,9 @@ class Triangle
   end
 
   def kind
-    true_triangle
+    if side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 > side3 || side2 + side3 > side1 || side1 + side3 > side2
+      raise TriangleError
+    else
       if side1 == side2 && side2 == side3
         :equilateral
       elsif side1 == side2 || side2 == side3 || side1 == side3
@@ -18,13 +20,8 @@ class Triangle
         :scalene
       end
     end
-
-  def true_triangle
-    if side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 > side3 || side2 + side3 > side1 || side1 + side3 > side2
-      raise TriangleError
-    end
-    end
   end
+
 
   class TriangleError < StandardError
 

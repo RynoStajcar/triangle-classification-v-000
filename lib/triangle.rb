a6244d1ad767f14ff8919e18ac0fbd8cb04602a9
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 22:35:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,12 +18,12 @@ class Triangle
         :scalene
       end
     end
-  end
 
   def true_triangle
     if side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 > side3 || side2 + side3 > side1 || side1 + side3 > side2
       raise TriangleError
     end
+  end
 
   class TriangleError < StandardError
 

f81a39eb37d8306f745e65c7eb0f32cc863f6993
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 22:50:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,11 +11,11 @@ class Triangle
   def kind
       validate_triangle
       if side1 == side2 && side2 == side3
-      :equilateral
-    elsif side1 == side2 || side2 == side3 || side1 == side3
-      :isosceles
-    else
-      :scalene
+        :equilateral
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        :isosceles
+      else
+        :scalene
     end
   end
 
